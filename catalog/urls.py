794d553347ff26ad6b4d2a3443fa41ae28811281
Your Name <you@example.com>
2025-08-06 11:06:46 +0300

    init_homework_branch_Caching_and_business_logic

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import HomeView, ProductDetailView, ContactsView
 from .views import ProductCreateView, ProductUpdateView, ProductDeleteView
+from django.views.decorators.cache import cache_page
 
 app_name = 'catalog'
 
@@ -11,4 +12,7 @@ urlpatterns = [
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+    path('product/<int:pk>/', cache_page(60 * 15)(ProductDetailView.as_view()),
+    path('category/<int:category_id>/', CategoryProductsView.as_view(), name='category_products'),
 ]
+
